6c4eda23fe0c12d625b987abceb4fb301576d8ce
gandghub <hackthehacker33@gmail.com>
2024-06-09 11:24:04 -0700

    AirBnB_clone_v3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """Init file for views module"""
+
 from flask import Blueprint
-
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
 
 from api.v1.views.index import *
 from api.v1.views.states import *
@@ -14,3 +11,4 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
