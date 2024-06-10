ed57efe321c38b32f73ed8f19c0da2685cabbb57
Zeinu Kedir <zeinuka641@gmail.com>
2024-06-09 18:06:29 -0700

    AirBnB clone - RESTful API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """ Index """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -10,3 +11,21 @@ def status():
     return jsonify({
         'status': 'OK'
     })
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    stats of all objs route
+    :return: json of all objs
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    return jsonify(data)
